0ead38d00cd4de42014f8287b2ffe5eba4984ddd
Bailan766 <2332405059@qq.com>
2026-06-27 20:05:33 +0800

    perf: v2.1.0 multi-thread parallel optimization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.apkagent"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10
-        versionName = "2.0.0"
+        versionCode = 11
+        versionName = "2.1.0"
         vectorDrawables { useSupportLibrary = true }
     }
 
